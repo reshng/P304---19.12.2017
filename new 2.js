87074b2d2eff04db80a097a2268603a21eb7e5d2
reshng <34545994+reshng@users.noreply.github.com>
2017-12-19 17:50:44 +0400

    Add files via upload

---
+++ new 2.js
@@ -0,0 +1,207 @@
+ 
+ // 1 ci MESELE: FIRST OR LAST?
+ // var number = Number(prompt("Enter Number"));
+
+
+// if(number >= 100 && number<= 999){
+// if(number%10 > (number- (number % 100)) /100 ) {
+	// document.write(number%10) ;
+	
+// }
+// else if(number%10 < (number- (number % 100)) /100) {
+	// document.write((number- (number % 100)) /100);
+// }
+// else if (number%10 == (number- (number % 100)) /100)
+// {
+	// document.write("=");
+// }
+// }
+
+ // MESELE 2: SEASON
+
+ // var number = Number(prompt("Number of month"));
+ // if(number >=1 && number<=12){
+  // if (number >=3 && number<=5){
+	  // document.write("Spring");
+	  
+  // }
+  // else if(number>=6 && number<=8){
+	  // document.write("Summer");
+  // }
+// else if(number>=9 && number<=11){
+	// document.write("Autumn");
+// }
+// else {
+	// document.write("Winter");
+	
+// }
+ // }
+ 
+ // MESELE 3: EVEN AND ODD NUMBERS
+ 
+ // var a = Number(prompt("Daxil Et"));
+ // var b = Number(prompt("Daxil Et"));
+ // var c = Number(prompt("Daxil Et"));
+ 
+ // if(((a%2==0 && b%2!=0) || (a%2!=0 && b%2==0)) || ((a%2==0 && c%2!=0) || (a%2!=0 && c%2==0)) || ((b%2==0 && c%2!=0) || (b%2!=0 && c%2==0)) ) {
+	 // document.write("YES");
+ // }
+ // else {
+	 // document.write("NO");
+ // }
+  
+    // MESELE 4: WARDROBE
+  // var a = Number(prompt("Daxil Et"));
+  // var b = Number(prompt("Daxil Et"));
+  // var c = Number(prompt("Daxil Et"));
+  // var x = Number(prompt("Daxil Et"));
+  // var y = Number(prompt("Daxil Et"));
+  
+  // if(x>=a && y>=c){
+	 // document.write("YES");
+  // }
+  // else {
+	// document.write("NO"); 
+  // }
+ 
+ //  MESELE 5 : TOURIST
+ 
+   // var k = Number(prompt("Daxil Et"));
+   // var w  = Number(prompt("Daxil Et"));
+   // var a1 = Number(prompt("Daxil Et"));
+   // var b1 = Number(prompt("Daxil Et"));
+   // var a2 = Number(prompt("Daxil Et"));
+  // var b2 = Number(prompt("Daxil Et"));
+  // var a3 = Number(prompt("Daxil Et"));
+  // var b3 = Number(prompt("Daxil Et"));
+  
+  // if(b1 == k && a1<=w){
+	  // document.write("YES")	  
+  // }
+  
+  // else if(b2 == k && a2<=w){
+	  // document.write("YES")	  ;
+  // }
+ // else if(b3 == k && a3<=w){
+	  // document.write("YES")	  ;
+  // }
+// else if(b1 + b2 == k && a1 + a2<=w){
+	  // document.write("YES")	  ;
+  // }
+// else if(b1 + b3 == k && a1 + a3<=w){
+	  // document.write("YES")	 ; 
+  // }
+ // else if(b2 + b3 == k && a2 + a3<=w){
+	  // document.write("YES")	;  
+  // }
+ 
+ // else if(b1 + b2 + b3 == k && a1 + a2 + a3<=w){
+	  // document.write("YES");	  
+  // }
+ // else {
+	 // document.write("NO");
+ // }
+ 
+ 
+ // MESELE 6: ODD DIVISORS
+ 
+ // var num = Number(prompt("Daxil et")) ;
+  
+  // var div = 1;
+  // if ((num) > 0) {
+  // while (div <= num ) {
+	  // if(parseInt(num)%div == 0) {
+		  // document.write(div+ " ") ;
+	  // }
+	
+	  // div=div +2;	  
+	  
+  
+  
+  // } }
+  
+//  MESELE 7: DIGITS
+  
+  // var num = Number(prompt("Enter number"));
+  // var count =0;
+  // var i = 1;
+  // if(num>=0){
+	  // while (num%10 >0) {
+		  // count++;
+		  // num = parseInt(num/10);
+		  
+		  
+  // } }
+	  // document.write(count);
+	  
+	  // MESELE 8: NUMBER OF DIVISORS:
+	  
+	  
+	  // var num = Number(prompt("Enter Number"));
+	  
+	  // var count = 0;
+	  // var div =1;
+	  // if(num>0){
+	  
+	  // while(div<=num){
+		  // if(num % div == 0){
+			  // count++ ;
+		  // }
+		  // div++;
+		  
+		  
+	  // }
+	   // document.write(count); 
+	  // }
+	 
+	  // else{
+		  // document.write("Duzgun reqemi gir") ;
+	  // }
+	  
+	  
+	  
+	   // var number = Number(prompt("Daxil Et"));
+	    
+	  // var count =0;
+	  // var digitSum =0;
+	  // var newNum = number; 
+	  
+	  // while(newNum>0){
+		 
+		 // while(number%10>0){
+			 
+			 // digitSum= digitSum + number%10;
+			 // number = parseInt(number/10);
+		 // }
+		 // newNum = newNum - digitSum;
+		 // count++;
+		  // number = newNum;
+		 // }
+	  
+	  // document.write(count);
+	  
+  var n = Number(prompt("Qefes sayi"));
+  
+  var m = Number(prompt("Qefes sayi"));
+ 
+document.write(m-n+1);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
